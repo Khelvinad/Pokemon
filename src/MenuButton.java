e052e4c7cc224b2489b5ffd53c670c6dbf0b7ad2
khelvinad <khelvinadit@gmail.com>
2025-05-26 22:07:12 +0700

    --alllow-empty-message

--- src/MenuButton.java
+++ src/MenuButton.java
@@ -41,4 +41,38 @@ abstract class MenuButtonAction {
             e.printStackTrace();
         }
     }
+
+    public JLabel createChatBoxLabel(String message,float sizeFont, int width, int height, int paddingX, int paddingY) {
+        String imagePath = "src/Asset/chatbox.png";
+        ImageIcon icon = new ImageIcon(imagePath);
+        Image scaledImage = icon.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        JLabel label = new JLabel(new ImageIcon(scaledImage));
+        label.setLayout(null);
+        label.setSize(width, height);
+        label.setOpaque(false);
+
+        JTextArea textArea = new JTextArea(message);
+        textArea.setBackground(Color.WHITE);
+        textArea.setWrapStyleWord(true);
+        textArea.setLineWrap(true);
+        textArea.setEditable(false);
+        textArea.setFocusable(false);
+        textArea.setOpaque(true);
+        textArea.setBackground(Color.WHITE);
+        textArea.setFont(customFont.deriveFont(Font.PLAIN, sizeFont));
+
+        int textWidth = width - 2 * paddingX;
+        int textHeight = height - 2 * paddingY;
+
+        int textX = (width - textWidth) / 2;  
+        int textY = (height - textHeight) / 2;
+        textArea.setBounds(textX, textY, textWidth, textHeight);
+
+        label.add(textArea);
+
+        return label;
+    }
+
+
+
 }
--- src/StartButtonAction.java
+++ src/StartButtonAction.java
@@ -2,6 +2,10 @@ import java.awt.*;
 import javax.swing.*;
 
 class StartButtonAction extends MenuButtonAction {
+    ImageIcon bulbaF;
+    ImageIcon charmender;
+    ImageIcon squirtle;
+
     public StartButtonAction(Frame frameApp) {
         super(frameApp);
     }
@@ -10,18 +14,45 @@ class StartButtonAction extends MenuButtonAction {
     public void execute() {
         frame.getContentPane().removeAll();
 
+        bulbaF = loadPokemon("/Asset/Pokemon/Bulba.png", 100);
+        charmender = loadPokemon("/Asset/Pokemon/charmender.png", 50);
+        squirtle = loadPokemon("/Asset/Pokemon/squirtle.png", 50);
+
         JPanel startPanel = new JPanel(new BorderLayout());
-        startPanel.setBackground(Color.CYAN);
+        startPanel.setOpaque(true);
+        startPanel.setBackground(Color.BLACK);
 
         JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         topPanel.setOpaque(false);
+
+        JPanel centerPanel = new JPanel();
+        centerPanel.setOpaque(false);
+
         JButton backButton = createBackButton();
         topPanel.add(backButton);
 
+        JLabel chatBox = createChatBoxLabel(
+            "Chose your POKETHOL", 20,
+            700, 200, 50, 40
+        );
+        chatBox.setHorizontalAlignment(SwingConstants.CENTER);
+        chatBox.setVerticalAlignment(SwingConstants.CENTER);
+        centerPanel.add(chatBox);
+
+        JLabel bulba = new JLabel(bulbaF);
+        centerPanel.add(bulba);
+
         startPanel.add(topPanel, BorderLayout.NORTH);
+        startPanel.add(centerPanel);
 
         frame.setContentPane(startPanel);
         frame.revalidate();
         frame.repaint();
+    }
+
+    private ImageIcon loadPokemon( String path, int scale){
+        ImageIcon rawIcon = new ImageIcon(getClass().getResource(path));
+        Image scaledImage = rawIcon.getImage().getScaledInstance(scale, scale, Image.SCALE_SMOOTH);
+        return new ImageIcon(scaledImage);
     }
 }
